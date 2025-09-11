c2282c4fe98c7ff0f1b4f373fe181f9428c1a56f
Shoaib2121 <shoaibi4642@gmail.com>
2025-09-11 15:16:38 +0500

    Your commit message here

--- api/netsuite.js
+++ api/netsuite.js
@@ -2,7 +2,8 @@
 // Place this file in /api/netsuite.js in your project root (not src)
 
 export default async function handler(req, res) {
-  const url = "https://td3032620.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=99&deploy=1&compid=TD3032620&ns-at=AAEJ7tMQZ7cccAU6W7MOeCTu5No9Jszw0CV_6cNhzByn8MXjXaU";
+  const url =
+    "https://td3032620.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=99&deploy=1&compid=TD3032620&ns-at=AAEJ7tMQZ7cccAU6W7MOeCTu5No9Jszw0CV_6cNhzByn8MXjXaU";
 
   try {
     const response = await fetch(url, {
@@ -12,7 +13,7 @@ export default async function handler(req, res) {
       },
     });
     const data = await response.json();
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
